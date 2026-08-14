d5c49d1518bae6e3f3493a91892c2e648834c418
Alina Mikhailova <alina.mik.kz@gmail.com>
2026-08-14 16:19:53 +1000

    feat: implement template composition and static assets initialization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,8 +8,11 @@ import (
 func main () {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /{$}", home)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
